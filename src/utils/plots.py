7c1e54ac8fec608447624af4407f46f63f2471dc
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-08-06 13:11:45 +0200

    increase plotting utils to brighten up images

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -1,13 +1,30 @@
-from matplotlib.pyplot import show, subplots
-from numpy import ndarray
+from matplotlib.pyplot import figure, show
+from numpy import minimum, ndarray
 
 
-def plot_image_and_prediction(image: ndarray, prediction: ndarray):
-    # Transpose the image and prediction to have the channels last
+def plot_image_and_prediction(
+    image: ndarray, prediction: ndarray, brighten: float = 1.0
+):
+    """
+    Plots the original image and the prediction side by side.
+
+    Parameters:
+        - image: The original RGB image.
+        - prediction: The predicted height map.
+        - brighten: A factor to brighten the image. Default is 1.0 (no change).
+    """
+
+    # Transpose the image and prediction to have channels last
     image = image[:3, :, :].transpose(1, 2, 0)
 
-    # Create a subplot to display the image and prediction side by side
-    _, (ax1, ax2) = subplots(1, 2, figsize=(12, 6))
+    # Brighten the image using the simplified method
+    image = minimum(image * brighten, 1)
+
+    # Create a custom grid for the image, prediction, and colorbar
+    fig = figure(figsize=(14, 6))
+    ax1 = fig.add_axes([0.05, 0.1, 0.45, 0.8])
+    ax2 = fig.add_axes([0.51, 0.1, 0.45, 0.8])
+    cax = fig.add_axes([0.97, 0.1, 0.01, 0.8])
 
     # Plot the original image
     ax1.imshow(image)
@@ -15,8 +32,13 @@ def plot_image_and_prediction(image: ndarray, prediction: ndarray):
     ax1.axis("off")
 
     # Plot the prediction
-    ax2.imshow(prediction.squeeze(), cmap="viridis")
+    im = ax2.imshow(prediction.squeeze(), cmap="viridis")
     ax2.set_title("Predicted Height Map")
     ax2.axis("off")
+
+    # Add colorbar to the dedicated axis
+    fig.colorbar(im, cax=cax, orientation="vertical").set_label(
+        "Height (meters)", rotation=-90, va="bottom"
+    )
 
     show()
