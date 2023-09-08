0d8f0c702788864e8bbf7002e31f2ee399579ff7
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-09-08 18:01:38 +0200

    adjust notebook

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from torch import Tensor, abs
+from torch import Tensor
 from torch.nn import HuberLoss
 
 
@@ -27,9 +27,12 @@ def loss(
     return fn(outputs, targets)
 
 
-def range_loss(outputs: Tensor, targets: Tensor, lower: int, upper: int) -> Tensor:
+def range_loss(
+    outputs: Tensor, targets: Tensor, lower: int, upper: int, delta: float = 3
+) -> Tensor:
     outputs, targets = filter(outputs, targets)
 
+    fn = HuberLoss("mean", delta)
     idx = (targets >= lower) & (targets < upper)
 
-    return abs(targets[idx] - outputs[idx]).mean()
+    return fn(outputs[idx], targets[idx])
