a40bcf77abb18bb55d53ca8a0aa63da6c0b3a462
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-09-08 16:52:28 +0200

    update main script

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
+# Imports
 import os
 from datetime import timedelta
 from logging import INFO, basicConfig, info
 from time import time
 
-from numpy import digitize, percentile
-from numpy.random import seed as npseed
-from segmentation_models_pytorch import Unet
+from numpy import argmax
 from sklearn.model_selection import train_test_split as split
 from torch import device as Device
 from torch.backends.mps import is_available as mps_available
 from torch.cuda import is_available as cuda_available
 from torch.optim import Adam
-from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
 
-# from src.models.unet import Unet
+from src.models.unet import Unet
 from src.utils.dataset import ForestDataset
 from src.utils.loss import loss
+from src.utils.misc import seed
 from src.utils.models import evaluation, save, training
 from src.utils.preprocessing import Preprocessor
+from src.utils.sampling import compute_sampling_weights
 
-patch_size = 128
+patch_size = 256
 img_dir = "data/images"
 log_dir = "logs"
 model_dir = "models"
 patch_dir = f"data/patches/{patch_size}"
 patches_file = f"data/patches/{patch_size}/info.fth"
 gedi_file = "data/gedi/gedi_complete.fth"
-seed = 42
-batch_size = 16
+random_state = 42
+batch_size = 12
 num_workers = 6
-learning_rate = 1e-4
+learning_rate = 1e-2
 epochs = 5
-ranges = [(i, i + 5) for i in range(0, 50, 5)]
+is_training = True
+bins = list(range(0, 55, 5))
 device = Device("cuda" if cuda_available() else "mps" if mps_available() else "cpu")
 
-npseed(seed)
-basicConfig(level=INFO, filename=os.path.join(log_dir, "main.log"), filemode="w")
+seed(random_state)
+
+basicConfig(level=INFO)
 
 info(f"Using {device} device")
 
@@ -45,55 +49,70 @@ if __name__ == "__main__":
     # Create preprocessor
     preprocessor = Preprocessor(patches_file, img_dir, patch_dir, gedi_file, patch_size)
 
+    # Run preprocessor
     preprocessor.run()
 
-    # Extract patches
-    patches = preprocessor.patches.sample(frac=0.5, random_state=seed)
+    # Get labels
+    labels = preprocessor.gedi.rh98
+
+    # Get patches
+    patches = preprocessor.patches
 
     info(f"Total number of patches: {len(patches)}")
 
-    # Create quantiles and stratify
-    quantiles = percentile(patches.n_labels, [25, 50, 75])
-    stratify = digitize(patches.n_labels, quantiles)
+    # Create stratification
+    stratify = patches.bins.apply(argmax)
 
     # Split patches
-    train, rest = split(patches, test_size=0.3, random_state=seed, stratify=stratify)
+    train, rest = split(
+        patches, test_size=0.3, random_state=random_state, stratify=stratify
+    )
 
+    # Create stratification for rest
     stratify = stratify[patches.index.isin(rest.index)]
 
-    val, test = split(rest, test_size=0.5, random_state=seed, stratify=stratify)
+    # Split rest
+    val, test = split(rest, test_size=0.5, random_state=random_state, stratify=stratify)
 
     # Create datasets
-    train_data = ForestDataset(train, patch_dir)
-    val_data = ForestDataset(val, patch_dir)
-    test_data = ForestDataset(test, patch_dir)
+    traindata = ForestDataset(train, patch_dir)
+    valdata = ForestDataset(val, patch_dir)
+    testdata = ForestDataset(test, patch_dir)
+
+    # Create weighted sampler
+    weights = compute_sampling_weights(train, labels, bins)
+    sampler = WeightedRandomSampler(weights, len(train))
 
     # Create dataloaders
-    train_loader = DataLoader(train_data, batch_size, True, num_workers=num_workers)
-    val_loader = DataLoader(val_data, batch_size, False, num_workers=num_workers)
-    test_loader = DataLoader(test_data, batch_size, False, num_workers=num_workers)
+    trainloader = DataLoader(
+        traindata, batch_size, False, sampler, num_workers=num_workers
+    )
+    valloader = DataLoader(valdata, batch_size, False, num_workers=num_workers)
+    testloader = DataLoader(testdata, batch_size, False, num_workers=num_workers)
 
-    # Create model
-    model = Unet(
-        encoder_name="inceptionv4",
-        encoder_weights=None,
-        in_channels=4,
-    ).to(device)
+    # Initialise model
+    model = Unet().to(device)
+
+    model_file = os.path.join(model_dir, f"m-{model.name}-p{patch_size}-e{epochs}.pt")
 
     info(model)
 
     # Create optimizer
     optimizer = Adam(model.parameters(), learning_rate)
 
+    # Create scheduler
+    scheduler = CosineAnnealingLR(optimizer, epochs)
+
     # Create writer
     writer = SummaryWriter(log_dir)
 
-    # Training loop
     start = time()
 
+    # Training loop
     for epoch in range(epochs):
-        training(train_loader, model, loss, device, writer, epoch, optimizer)
-        evaluation(val_loader, model, loss, device, writer, epoch)
+        training(trainloader, model, loss, device, writer, epoch, optimizer)
+        evaluation(valloader, model, loss, device, writer, epoch)
+        scheduler.step()
 
     end = time()
 
@@ -101,10 +120,11 @@ if __name__ == "__main__":
 
     info(f"Training completed in {timedelta(seconds=(end - start))}")
 
-    score = evaluation(test_loader, model, loss, device)
+    score = evaluation(testloader, model, loss, device)
 
     info(f"Final loss on test set: {score}")
 
     info(f"Saving model {model.name}")
 
-    save(model, os.path.join(model_dir, f"{model.name}.pt"))
+    # Save model
+    save(model, model_file)
