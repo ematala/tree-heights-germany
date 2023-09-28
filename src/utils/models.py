f137ec4a69ef01d1a65c176ab42ae23d33d44885
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-09-28 22:33:27 +0200

    resolve import error

--- src/utils/models.py
+++ src/utils/models.py
@@ -6,7 +6,7 @@ from torch import load as tload
 from torch import save as tsave
 from torch.nn import Module
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import LRScheduler
+from torch.optim.lr_scheduler import _LRScheduler as LRScheduler
 from torch.utils.data import DataLoader
 
 
