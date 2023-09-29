9a7780279ffd4b296c92cb98ec222a65b9e8a29e
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-09-29 10:51:36 +0200

    update training script

--- train.sh
+++ train.sh
@@ -6,12 +6,12 @@ source ./.env
 read -p "Please select model [vitnet=v/unet=u]: " model
 
 if [ "$model" == "v" ]; then
-    python3 -m src.models.vitnet.train
+    python3 -m src.models.vitnet.train > logs/train-v.log
 elif [ "$model" == "u" ]; then
-    python3 -m src.models.unet.train
+    python3 -m src.models.unet.train > logs/train-u.log
 else
     echo "Model unknown."
     exit 1
 fi
 
-curl --request POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --form "chat_id=${CHAT_ID}" --form "text=Finished training"
+curl --request POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument" --form "chat_id=${CHAT_ID}" --form "document=@logs/train-${model}.log" --form "caption=Finished training"
