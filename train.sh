d6291037a955e36af4ed19f69588c1c9e4c60593
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-11-24 15:42:58 +0100

    adjust batch size in bash training script

--- train.sh
+++ train.sh
@@ -3,7 +3,5 @@
 python3 train.py --model unet --batch_size 128
 python3 train.py --model vit-nano --batch_size 256
 python3 train.py --model vit-nano --batch_size 256 --teacher unet
-python3 train.py --model vit-micro --batch_size 128
-python3 train.py --model vit-tiny --batch_size 64
-
-python3 evaluate.py --batch_size 128
+python3 train.py --model vit-micro --batch_size 256
+python3 train.py --model vit-tiny --batch_size 128
